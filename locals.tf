8245390c0fee01805fc1ca6c1188c709a616a93d
Malo Marrec <malo.marrec@gmail.com>
2022-06-25 22:17:09 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -5,6 +5,5 @@ locals {
         Environment = var.environment
         ServiceCatalogId = 1
         ServiceName = "nuk-backstage"
-        Repository = "https://github.com/newsuk/nuk-backstage"
     }
 }
